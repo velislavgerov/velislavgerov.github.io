eefe6b9705b7ff87c320d10ea514da4e1e0383fb
thunderstruck47 <velislav.gerov.12@aberdeen.ac.uk>
2021-10-14 22:08:01 +0300

    Don't need that anymore, we're using tailwindcss

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,30 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
   return (
     <main class="min-h-screen bg-yellow-50 bg-opacity-50 text-gray-900 dark:bg-gray-800 dark:bg-opacity-100 dark:text-white">
